49b267047a78ac2196ad950d91f24eab3a863436
Jason Swann <jpswann@Jasons-MacBook-Pro.local>
2024-08-07 17:08:59 -0400

    backend

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.use("/", (req, res) => res.send("API Running"));
+// Connect DB
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-
-//test
