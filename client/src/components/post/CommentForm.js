a71e6c82e443ef0acb6aa0540c75764df002a8ca
Jason Swann <jpswann@Jasons-MacBook-Pro.local>
2024-08-15 17:03:40 -0400

    test

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import Spinner from "../layout/Spinner";
 import { addComment } from "../../actions/post";
 
 const CommentForm = ({ postId, addComment }) => {
