bac10d1047144f68dfed18aa127ad8571605f39b
Jason Swann <jpswann@Jasons-MacBook-Pro.local>
2024-08-15 17:35:34 -0400

    test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
